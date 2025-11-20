5eed619fb75fd511bf40da8f7666be7a1e619549
mukulsingh24 <rmks1004@gmail.com>
2025-11-20 21:22:49 +0530

    router installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react"
+import Nav from "./components/navbar"
+import { BrowserRouter,Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <div className="App">
-      
+        <Nav />
     </div>
   )
 }
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter,Routes } from "react-router-dom"
+function Nav(){
+    return(
+        <div>
+            <h1>E-Commerce Store</h1>
+        </div>
+    )
+}
+
+export default Nav
