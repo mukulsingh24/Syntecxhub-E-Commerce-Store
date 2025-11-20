2e72ba4c66dd70acfad69c79bfcb743ee7bc78c3
mukulsingh24 <rmks1004@gmail.com>
2025-11-20 22:14:08 +0530

    backend server created

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
+require('dotenv').config({path: './'})
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const app = express()
+app.use(cors())
+const port = 5000
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database Connected")
+}).catch(err){
+    console.log("Database Not Connected",err)
+}
+
+app.listen(port,()=>{
+    console.log("Server is Running")
+})
