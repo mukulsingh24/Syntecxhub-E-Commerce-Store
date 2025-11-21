09610395f4dbcf9d299b78f071261c69c6b78064
mukulsingh24 <rmks1004@gmail.com>
2025-11-21 11:41:50 +0530

    regitseration done (frontend + backend)

--- frontend/src/pages/registration.jsx
+++ frontend/src/pages/registration.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from 'axios'
+import { Form } from "react-router-dom";
 function Register (){
+    const[name,setName] = useState("");
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        try{
+        const response = await axios.post("http://localhost:5000/api/v1/user/register",{
+            name,email,password}
+        )
+        alert("Registration Successful!")}
+        catch(err){
+            alert("User Already Exists")
+        }
+    }
     return(
         <div>
-
+            <div className="register-container">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Enter Name"  onChange={(e)=>setName(e.target.value)} />
+                    <input type="email" placeholder="Enter Email"  onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="password" placeholder="Enter Password"  onChange={(e)=>setPassword(e.target.value)}/>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
         </div>
     )
 }
