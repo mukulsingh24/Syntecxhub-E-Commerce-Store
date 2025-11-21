2bea859ffef8a95cb159199a0817b050d1e05268
mukulsingh24 <rmks1004@gmail.com>
2025-11-21 10:28:28 +0530

    howepage working(fornend + backend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import Nav from "./components/navbar"
-import { BrowserRouter,Routes } from "react-router-dom"
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/homepage"
+import Login from "./pages/loginpage"
+import Register from "./pages/registration"
 function App() {
 
   return (
-    <div className="App">
-    </div>
+    <>
+        <Routes>
+           <Route path="/" element={ <Home /> } />
+           <Route path="/login" element={ <Login /> } />
+           <Route path="/register" element={ <Register/> } />
+        </Routes>
+     </>
   )
 }
 
--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,19 +1,18 @@
 import React, { useState,useEffect } from "react";
 import { BrowserRouter,Routes } from "react-router-dom"
-
+import axios from 'axios'
 function Home(){
     const[products,setProducts] = useState([]);
     useEffect(()=>{
-        const obj = [{
-            _id : "01",
-            name:"Keyboard",
-            image:"url",
-            price:2000
-        }]
-        setProducts(obj)
+        const fetch = async() =>{
+            const response = await axios.get("http://localhost:5000/api/v1/products")
+            setProducts(response.data.get)
+        }
+        fetch()
     },[])
     return(
         <div>
+            <h1>This is Sample</h1>
             <ul>
             {products.map((item,index) => (
                 <li key={index}>{item.name},{item.price}
