9594a809792142a9729023f1c75ea4713cd8a61a
mukulsingh24 <rmks1004@gmail.com>
2025-11-21 10:39:26 +0530

    user model created

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+})
+module.exports = mongoose.model('User',UserSchema)
